28370e506416f3a6244bb9b51830ba7bfe61f718
Creator5923 <nlokhande5923@gmail.com>
2020-12-28 20:10:13 +0530

    addded status table for py code

---
+++ MaxProfitFromShare.cpp
@@ -0,0 +1,15 @@
+//Maximum profit by buying and selling a share at most twice
+//we will use the same approach as we done in max profit on buying and selling share on a single day
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int size;
+	cin>>size;
+	int arr[size];
+	for(int i=0;i<size;i++)
+	cin>>arr[i];
+	int profit = INT_MIN;
+	
+}
